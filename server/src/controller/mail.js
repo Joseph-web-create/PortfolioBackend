f90f9212079e25cadb6a30308601181a7f7c1a7f
Aace <aacejovi@gmail.com>
2025-10-08 10:46:26 +0100

    false

--- server/src/controller/mail.js
+++ server/src/controller/mail.js
@@ -19,9 +19,9 @@ export const sendMails = async (req, res) => {
 
   try {
     const transport = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      service: "gmail",
       port: 465,
-      secure: true,
+      secure: false,
       auth: {
         user: myEmail,
         pass: googleAppPassword,
