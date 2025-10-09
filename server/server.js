2ef819aee6151ca85719bb60e9a90bb08b08bd5a
Aace <aacejovi@gmail.com>
2025-10-09 14:22:04 +0100

    frontend url

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://portfolio-chi-six-jnpbqearet.vercel.app",
+  ],
   optionsSuccessStatus: 200,
   method: ["GET", "POST", "PATCH", "DELETE"],
   credential: true,
